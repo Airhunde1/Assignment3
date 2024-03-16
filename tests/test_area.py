8bdfc6995da03d6b4eb491b375038fd24613016b
Mary Oghogho Airhun'de <42832876+Airhunde1@users.noreply.github.com>
2024-03-15 20:09:34 -0400

    Update test_area.py

--- tests/test_area.py
+++ tests/test_area.py
@@ -6,8 +6,7 @@ def test_calculate_area_square_negative():
         calculate_area_square(-2)
 
 def studentnumber():
-      # my student number is: 100953904
-     assert calculate_area_square(2) == 4 
+    assert calculate_area_square(2) == 4 
 
   
 def test_calculate_area_square_string():  
@@ -17,3 +16,8 @@ def test_calculate_area_square_string():
 def test_calculate_area_square_list():  
     with pytest.raises(TypeError):  
         calculate_area_square([2])
+
+def test_calculate_area_square():  
+    assert calculate_area_square(2) == 4  
+    assert calculate_area_square(2.5) == 6.25
+    assert calculate_area_square(3) == 9
