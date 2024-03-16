d4b6c6056c8d8e5887c64d029925d6561b3f3987
Mary Oghogho Airhun'de <42832876+Airhunde1@users.noreply.github.com>
2024-03-15 20:07:13 -0400

    Update test_area.py

--- tests/test_area.py
+++ tests/test_area.py
@@ -17,8 +17,3 @@ def test_calculate_area_square_string():
 def test_calculate_area_square_list():  
     with pytest.raises(TypeError):  
         calculate_area_square([2])
-
-def test_calculate_area_square():  
-    assert calculate_area_square(2) == 4  
-    assert calculate_area_square(2.5) == 6.25
-    assert calculate_area_square(3) == 10 
